de1ae7d0a74054644380e704456ce8fd483197cd
auge <dev@auge.in.ua>
2016-10-17 02:25:13 +0300

    + wrapper

---
+++ src/lib/core/wrapper.js
@@ -0,0 +1,8 @@
+import Wrapper from '../classes/wrapper/constructor';
+import '../classes/wrapper/prototype';
+
+function wrapper(selector) {
+	return new Wrapper(selector);
+}
+
+export default wrapper;
