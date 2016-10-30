67efe61a8ef5a1cfd457813e75498c0b93dcc1c1
auge <dev@auge.in.ua>
2016-10-30 13:31:09 +0200

    + updated GET method for map-object

--- src/lib/classes/locales/get.js
+++ src/lib/classes/locales/get.js
@@ -1,6 +1,8 @@
+import Locale from '../locale/constructor';
+
 export default function get(key) {
 	if (!this._data.has(key)) {
-		this._data.set(key, new Map());
+		this._data.set(key, new Locale());
 	}
 	return this._data.get(key);
 }
