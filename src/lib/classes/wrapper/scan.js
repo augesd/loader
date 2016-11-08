55b2a0174558313a2a3571a235eca6070446de78
auge <dev@auge.in.ua>
2016-11-09 00:53:16 +0200

    + use dependencies

--- src/lib/classes/wrapper/scan.js
+++ src/lib/classes/wrapper/scan.js
@@ -1,13 +1,14 @@
-import { hooks } from '../../utils/hooks';
+import {hooks} from '../../utils/hooks';
 
 function scan(force) {
 	// Return the promise
+	
 	return new Promise((resolve, reject) => {
 		if (!this.selector) reject(hooks.t('errors.wrongSelector'));
-
+		
 		let res = hooks.$(this.selector + (force ? '' : ':not([data-ready="1"])'));
 		res.data('ready', '1');
-
+		
 		resolve(res);
 	});
 }
